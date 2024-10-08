f6f5c965db169ffa76111af75bf0f4c1e4dd9f1b
M.D.W.De Silva <wdulanjith@gmail.com>
2024-10-08 16:10:35 +0530

    game board updated
    Best practice on updating Object state immutably

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player.jsx";
 
 function App() {
-    return (
-        <>
-            <main>
-                <div id="game-container">
-                    <ol id="players">
-                        <Player initialName="Player 1" symbol="X" />
-                        <Player initialName="Player 2" symbol="O" />
-                    </ol>
-                    <GameBoard />
-                </div>
-                LIST
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <Player initialName="Player 1" symbol="X" />
+            <Player initialName="Player 2" symbol="O" />
+          </ol>
+          <GameBoard />
+        </div>
+        LIST
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,39 +1,32 @@
 import { useState } from "react";
 
 export default function Player({ initialName, symbol }) {
-    const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick() {
-        setIsEditing((editing) => !editing);
-    }
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
 
-    function handleChange(event) {
-        setPlayerName(event.target.value);
-    }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
-    let player = <span className="player-name">{playerName}</span>;
+  let player = <span className="player-name">{playerName}</span>;
 
-    if (isEditing) {
-        player = (
-            <input
-                type="text"
-                required
-                value={playerName}
-                onChange={handleChange}
-            />
-        );
-    }
-
-    return (
-        <li>
-            <span className="player">
-                {player}
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleEditClick}>
-                {isEditing ? "Save" : "Edit"}
-            </button>
-        </li>
+  if (isEditing) {
+    player = (
+      <input type="text" required value={playerName} onChange={handleChange} />
     );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {player}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
